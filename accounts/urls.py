815a61f744ec34d4ef0f7b31e59e4fc8a066e06d
wooyeol <dnduf158@gmail.com>
2022-11-09 15:22:50 +0900

    setting : 기본 세팅

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,13 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from . import views
+
+app_name = ""
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.main, name='name'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+]
