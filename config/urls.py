04cf308b1cf23801dd5ef5a1caf9eb97757fc9df
an dong woo <akskwha@naver.com>
2022-11-10 12:05:09 +0900

    fix : Config/urls 오류 수정 [안동우]

--- config/urls.py
+++ config/urls.py
@@ -14,24 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path,include
-=======
 from django.urls import path, include
->>>>>>> cb6b013efa33b76024efb4f9c0ef64094229dea1
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('admin/', admin.site.urls),
-    path('', views.main, name='name'),
     path('community/', include('community.urls')),
-=======
     path("admin/", admin.site.urls),
     path("", views.main, name="name"),
     path("products/", include("products.urls")),
     path('accounts/', include('accounts.urls')),
->>>>>>> cb6b013efa33b76024efb4f9c0ef64094229dea1
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
