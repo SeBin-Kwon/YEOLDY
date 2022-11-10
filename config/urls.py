1eb13bde77d1debd560789ff70b68fcbaf648b3f
sebin kwon <sbkwon98@gmail.com>
2022-11-10 14:47:30 +0900

    style : url,views,base.html 수정

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main, name='name'),
+    path('', views.main, name='main'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
