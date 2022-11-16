eecbf3e2adde9621c4bd5b490efefbf2b1437ccb
yodayeong <duekdud01@naver.com>
2022-11-16 17:46:26 +0900

    fix : Kakaopay/장바구니와 연결 완료 [여다영]

--- cart/views.py
+++ cart/views.py
@@ -18,6 +18,7 @@ def add_cart(request, product_id):
 
     # cart_item이 있는지 없는지 여부 확인
     try:
+        #size와 color까지 같은 애들은 수량만 증가해줌
         cart_item = CartItem.objects.get(product=product, color=color, size=size, user=request.user)
         cart_item.quantity += int(cart_quantity)
         cart_item.save()
@@ -34,6 +35,7 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
     try:
         cart_items = CartItem.objects.filter(user=request.user)
         for cart_item in cart_items:
+            #총 가격과 총 개수를 계산
             total += cart_item.product.cost * cart_item.quantity
             counter += cart_item.quantity
     except ObjectDoesNotExist:
@@ -47,12 +49,14 @@ def cart_detail(request, total=0, counter=0, cart_items=None):
 
 @login_required
 def delete_cart(request, product_id, product_color, product_size):
+    #해당 유저, 상품, 가격, 사이즈까지 모두 같은 애들만 삭제
     cart_item = CartItem.objects.get(id=product_id, color=product_color, size=product_size, user=request.user)
     cart_item.delete()
     return redirect("cart:cart_detail")
 
 @login_required
 def clear_cart(request):
+    #해당 유저의 item들을 모두 삭제
     cart_items = CartItem.objects.filter(user=request.user)
     cart_items.delete()
     return redirect("cart:cart_detail")
