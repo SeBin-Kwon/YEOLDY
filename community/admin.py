7e8e290999b304ff4bc9a86e895aff74144dd139
an dong woo <akskwha@naver.com>
2022-11-19 20:53:23 +0900

    feat : admin.py 모델 등록 [안동우]

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import QnA,Review,Photo
 # Register your models here.
+admin.site.register(QnA)
+admin.site.register(Review)
+admin.site.register(Photo)
--- kakaopay/admin.py
+++ kakaopay/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import OrderListFinal,OrderList
 # Register your models here.
+admin.site.register(OrderListFinal)
+admin.site.register(OrderList)
+
